720a5f24feba83725ec94bdd683391d74ea95c87
Yesica Arias <jesicaarias271@gmail.com>
2026-06-12 11:09:08 -0300

    añado registroctividad y modifico useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { Container } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
-import ListaProyectos from './components/ListaProyectos';
+import "./css/styles.css";
+
 import Header from './components/header';
 import Footer from './components/footer';
 import Nav from './components/nav';
-import "./css/styles.css";
+
+import ListaProyectos from './components/ListaProyectos';
+import DetalleProyecto from './components/DetalleProyecto';
 import PerfilUsuario from './components/PerfilUsuario';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Container maxWidth="lg">
       <Header />
       <Nav />
-      <PerfilUsuario />
-      <ListaProyectos />
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/proyectos" element={<ListaProyectos />} />
+        <Route path="/proyectos/:id" element={<DetalleProyecto />} />
+        <Route path="/perfil" element={<PerfilUsuario />} />
+      </Routes>
+
       <Footer />
     </Container>
   );
